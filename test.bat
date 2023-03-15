ff4761484afb1b47f70b710500149d719db7c3bd
leander-hyphen <124282749+leander-hyphen@users.noreply.github.com>
2023-03-15 17:19:09 +0100

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,3 +1,15 @@
+@echo off
+
+set "SELF=%~dpnx0"
+
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/leander-hyphen/test/main/test.bat', '%~dp0\test.bat')"
+
+if not "%SELF%"=="%~dp0\test.bat" (
+    start "" "%~dp0\test.bat"
+    exit /b
+)
+
+echo off
 title test.bat
 echo test.bat
 pause >nul
