7c59065572b1a95d783d508acfd4f5cfa6b67afa
leander-hyphen <124282749+leander-hyphen@users.noreply.github.com>
2023-03-15 17:34:08 +0100

    Update test.bat

--- test.bat
+++ test.bat
@@ -2,14 +2,20 @@
 
 set "SELF=%~dpnx0"
 
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/leander-hyphen/test/main/test.bat', '%~dp0\test.bat')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/leander-hyphen/test/raw/main/test.bat', '%~dp0\test.bat')"
+
+ping -n 1 -w 1000 127.0.0.1 >nul
 
 if not "%SELF%"=="%~dp0\test.bat" (
+    echo Updating now...
+    timeout /t 5 /nobreak >nul
     start "" "%~dp0\test.bat"
     exit /b
+) else (
+    echo This batch file has installed the latest version..
 )
 
-echo off
-title 0..bat
-echo 0.bat
-pause >nul
+echo Welcome to my batch file!
+
+:: Your batch file commands here
+echo Updated Succesfully!
